7c27aa7ef68af299f2245fafa34f813475db3fd4
Bishnu98084 <bishnugiri5423@gmail.com>
2026-05-28 12:02:28 +0300

    Added Admin and deserialize it

--- Json/Program.cs
+++ Json/Program.cs
@@ -1,6 +1,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 
 class Program
@@ -12,47 +13,69 @@ class Program
         public int Age { get; set; }
         public required string City { get; set; }
     }
-    public class Admin : User
+    public class Admin: User
     {
         public required string Role { get; set; }
     }
 
     static void Main(string[] args)
     {
-        string filePath = "/Users/bishnugiri/Desktop/OOP/Json/users.json";
+        // string filePath = "/Users/bishnugiri/Desktop/OOP/Json/users.json";
 
         // Read JSON file
-        string jsonData = File.ReadAllText(filePath);
-        XMLReaderExample.ReadXML();
+        // string jsonData = File.ReadAllText(filePath);
+        // XMLReaderExample.ReadXML();
 
-        // Deserialize JSON array into List<User>
-        List<User> users =
-            JsonConvert.DeserializeObject<List<User>>(jsonData) ?? new List<User>();
+        string adminJson =
+            File.ReadAllText("/Users/bishnugiri/Desktop/OOP/Json/admins.json");
 
-        // Add new entry
-        users.Add(new User
-        {
-            Name = "Michael",
-            Age = 28,
-            City = "Berlin"
-        });
+        List<Admin> admins =
+            JsonConvert.DeserializeObject<List<Admin>>(adminJson)
+            ?? new List<Admin>();
 
-        // Convert back to JSON
-        string updatedJson =
-            JsonConvert.SerializeObject(users, Formatting.Indented);
-
-        // Save updated JSON
-        File.WriteAllText(filePath, updatedJson);
-
-        // LOOP through all users
-        Console.WriteLine("All Users:");
-
-        foreach (User user in users)
+        foreach (Admin user in admins)
         {
             Console.WriteLine("----------------");
             Console.WriteLine("Name: " + user.Name);
             Console.WriteLine("Age: " + user.Age);
             Console.WriteLine("City: " + user.City);
+            Console.WriteLine("Role: " + user.Role);
         }
+        // Deserialize JSON array into List<User>
+        // List<User> users =
+        //     JsonConvert.DeserializeObject<List<User>>(jsonData) ?? new List<User>();
+
+        // // Add new entries
+        // users.Add(new User
+        // {
+        //     Name = "Michael",
+        //     Age = 28,
+        //     City = "Berlin"
+        // });
+        // users.Add(new User
+        // {
+        //     Name = "Sarah",
+        //     Age = 24,
+        //     City = "Madrid"
+        // });
+
+        // // Convert back to JSON
+        // string updatedJson =
+        //     JsonConvert.SerializeObject(users, Formatting.Indented);
+
+        // // Save updated JSON
+        // File.WriteAllText(filePath, updatedJson);
+
+        // // LOOP through all users
+        // Console.WriteLine("All Users:");
+
+        // foreach (User user in users)
+        // {
+        //     Console.WriteLine("----------------");
+        //     Console.WriteLine("Name: " + user.Name);
+        //     Console.WriteLine("Age: " + user.Age);
+        //     Console.WriteLine("City: " + user.City);
+        // }
+    
     }
 }
